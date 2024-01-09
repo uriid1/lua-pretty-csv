dbb5da6fffe24baf7f3cc747e2d66077e0988aa5
uriid1 <appdurov@gmail.com>
2024-01-09 18:44:19 +0300

    Release

--- pretty-csv.lua
+++ pretty-csv.lua
@@ -9,61 +9,136 @@ local utf8 = require('utf8')
 local COLOR = {
   reset ="\27[0m",
   gray = "\27[38;5;240m",
+  gray_number = "\27[38;5;245m",
 }
-local function colorize(color, text)
-  return color..text..COLOR.reset
-end
 
-local function readFile(filepath)
-  local fd = io.open(filepath, 'r')
-  if not fd then
-    return nil
+local function cprint(text, color, nocolor)
+  if nocolor then
+    io.write(text)
   end
-  local data = fd:read('*a')
-  fd:close()
 
-  return data
+  io.write(color..text..COLOR.reset)
 end
 
-local function parse(csv_data, csv_sep)
-  local result = {}
-  local index = 0
+local special = {
+  ['\a'] = true,
+  ['\b'] = true,
+  ['\t'] = true,
+  ['\n'] = true,
+  ['\v'] = true,
+  ['\f'] = true,
+  ['\r'] = true,
+}
+
+-- http://lua-users.org/lists/lua-l/2014-04/msg00590.html
+local function utf8_sub(s,i,j)
+   i = i or 1
+   j = j or -1
+
+   if (i < 1) or (j < 1) then
+      local n = utf8.len(s)
+      if not n then
+        return nil
+      end
+
+      if i < 0 then i = n + 1 + i end
+      if j < 0 then j = n + 1 + j end
+      if i < 0 then i = 1 elseif i > n then i = n end
+      if j < 0 then j = 1 elseif j > n then j = n end
+   end
+   if j < i then return '' end
+   i = utf8.offset(s, i)
+   j = utf8.offset(s, j + 1)
+   if i and j then return s:sub(i, j - 1)
+      elseif i then return s:sub(i)
+      else return ''
+   end
+end
+
+local function parse(filepath, param)
+  local csv = {}
+  local rowLen = {}
+  local csvIndex = 0
+
+  local sep = param.sep or ','
+  local limit = param.limit or -1
+  local max_word_len = param.word_len
 
   local rowCount = 0
-  for column in string.gmatch(csv_data, '.-\n') do
+  for column in io.lines(filepath, "*l") do
     local inQuotes = false
     local resRowStr = ''
-    for i = 1, #column do
-      local char = column:sub(i, i)
-      if char == csv_sep and not inQuotes then
+    local curLen = 0
 
-        index = index + 1
-        if not result[index] then
-          result[index] = {}
-        end
-        table.insert(result[index], resRowStr)
+    local columnLen = #column
+    for i = 1, columnLen do
+      local char = utf8_sub(column, i, i)
 
-        resRowStr = ''
-        goto continue
-      end
       if char == '"' then
         inQuotes = not inQuotes
       end
-      resRowStr = resRowStr .. char
-      ::continue::
+
+      if char == sep and not inQuotes then
+        csvIndex = csvIndex + 1
+
+        if max_word_len and curLen > max_word_len then
+          resRowStr = utf8_sub(resRowStr, 1, max_word_len - 3)..'...'
+          curLen = max_word_len
+        end
+
+        if not csv[csvIndex] then
+          csv[csvIndex] = {}
+          rowLen[csvIndex] = {}
+        end
+        table.insert(csv[csvIndex], resRowStr)
+        table.insert(rowLen[csvIndex], curLen)
+
+        curLen = 0
+        resRowStr = ''
+      else
+        if not special[char] then
+          resRowStr = resRowStr .. char
+          curLen = curLen + 1
+        end
+      end
+    end
+
+    csvIndex = csvIndex + 1
+
+    local strLen = utf8.len(resRowStr)
+    if max_word_len and strLen > max_word_len then
+      resRowStr = utf8_sub(resRowStr, 1, max_word_len - 3)..'...'
+    end
+
+    if not csv[csvIndex] then
+      csv[csvIndex] = {}
+      rowLen[csvIndex] = {}
+    end
+    table.insert(csv[csvIndex], resRowStr)
+    table.insert(rowLen[csvIndex], strLen)
+
+    if limit ~= -1 and limit == rowCount then
+      break
     end
 
     rowCount = rowCount + 1
-    index = 0
+    csvIndex = 0
   end
 
-  return result, rowCount
+  local result = {
+    csv = csv,
+    row_count = rowCount,
+    row_len = rowLen,
+    param = param,
+  }
+
+  return result
 end
 
-local function getMaxRowLen(csvTable, column)
+local function getMaxRowLen(csv, column)
   local maxLen = 0
-  for i = 1, #csvTable[column] do
-    local len = utf8.len(csvTable[column][i])
+  for i = 1, #csv[column] do
+    local len = csv[column][i]
     if len > maxLen then
       maxLen = len
     end
@@ -71,55 +146,94 @@ local function getMaxRowLen(csvTable, column)
   return maxLen
 end
 
-local function render(csvTable, rowCount)
-  local csvTableLen = #csvTable
-  local result = ''
+local function render(csv_table)
+  local csvTableLen = #csv_table.csv
+  local rowCount = csv_table.row_count
+  local csv = csv_table.csv
 
+  --
+  -- Header
+  --
+  cprint('┌', COLOR.gray)
+  local maxLen = {}
+  for i = 1, #csv_table.row_len do
+    local len = getMaxRowLen(csv_table.row_len, i)
+    table.insert(maxLen, len)
+
+    local pos = ' '..i ..' '
+    cprint(pos, COLOR.gray_number)
+    cprint(string.rep('─', len - #pos + 2), COLOR.gray)
+
+    if i == #csv_table.row_len then
+      cprint('┐', COLOR.gray)
+    else
+      cprint('┐', COLOR.gray)
+    end
+  end
+  io.write('\n')
+
+  --
+  -- Body
+  --
   for item = 1, rowCount do
     local lenRows = {}
     for column = 1, csvTableLen do
-      local maxLen = getMaxRowLen(csvTable, column)
-      local curItem = csvTable[column][item]
+      local maxLen = maxLen[column]
+      local curItem = csv[column][item]
       if not curItem then
         goto continue
       end
 
-      result = result .. colorize(COLOR.gray, '│ ')
+      cprint('│ ', COLOR.gray)
 
       local len = maxLen - utf8.len(curItem) + 1
       table.insert(lenRows, maxLen)
 
-      result = result .. curItem..string.rep(' ', len)
+      io.write(curItem..string.rep(' ', len))
 
       if column == csvTableLen then
-        result = result .. colorize(COLOR.gray, '│')
+        cprint('│', COLOR.gray)
+        cprint(' '..item, COLOR.gray_number)
       end
 
       ::continue::
     end
 
-    result = result .. '\n'
-    result = result .. colorize(COLOR.gray, '├')
+    io.write('\n')
 
-    for i = 1, #lenRows do
-      local len = lenRows[i]
+    if item ~= rowCount then
+      cprint('├', COLOR.gray)
+      for i = 1, #lenRows do
+        local len = lenRows[i]
 
-      result = result .. colorize(COLOR.gray, string.rep('─', len + 2))
-      if i == #lenRows then
-        result = result .. colorize(COLOR.gray, '┤')
-      else
-        result = result .. colorize(COLOR.gray, '┼')
+        cprint(string.rep('─', len + 2), COLOR.gray)
+        if i == #lenRows then
+          cprint('┤', COLOR.gray)
+        else
+          cprint('┼', COLOR.gray)
+        end
       end
-    end
 
-    result = result .. '\n'
+      io.write('\n')
+    end
   end
 
-  io.write(result)
+  --
+  -- End
+  --
+  cprint('└', COLOR.gray)
+  for i = 1, #maxLen do
+    cprint(string.rep('─', maxLen[i] + 2), COLOR.gray)
+
+    if i == #csv_table.row_len then
+      cprint('┘', COLOR.gray)
+    else
+      cprint('┴', COLOR.gray)
+    end
+  end
 end
 
 return {
-  readFile = readFile,
   parse = parse,
   render = render,
 }
--- test/main.lua
+++ test/main.lua
@@ -1,21 +1,14 @@
 local csv = require('pretty-csv')
 
-local csv_data = csv.readFile('payment_utf8.csv')
-if not csv_data or csv_data == '' then
-  print('Eror read: payment_utf8.csv')
-  os.exit(0)
-end
-
-local csvTable, rowCount = csv.parse(csv_data, ',')
+local data = csv.parse('test.csv', { sep = ',', limit = 20, word_len = 30 })
+csv.render(data)
 
 local total_sum = 0
-for i = 1, #csvTable[6] do
-  local item = csvTable[6][i]
-  if csvTable[8][i] == 'Зачислен' then
-    local sum = (tonumber(item:match('(%d+)')) or 0)
-    total_sum = total_sum + sum
-  end
+for i = 1, #data.csv[6] do
+ local item = data.csv[6][i]
+ if data.csv[8][i] == 'Зачислен' then
+   local sum = (tonumber(item:match('(%d+)')) or 0)
+   total_sum = total_sum + sum
+ end
 end
-
-csv.render(csvTable, rowCount)
 print('\n Total RUB: '..total_sum)
